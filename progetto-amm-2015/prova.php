f5869975c88541e4364b7144ae0581145fe465e9
marcoguria <marco.guria@tiscali.it>
2016-05-21 11:41:41 +0200

    Create prova.php

---
+++ progetto-amm-2015/prova.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Progetto di Amministrazione di Sistema 2015</title>
+    </head>
+    <body>
+        <h1>prova di lettura</h1>
+        <p>
+        ciao prova
+        </p>
+<p>
+<h3>Utenti</h3>
+<ul>
+    <li> Credenziali Gestore
+        <ul>
+            <li>username: gestore</li>
+            <li>password: guria</li>
+        </ul>
+    </li>
+    <li>Credenziali Clienti
+        <ul>
+            <li>username: cliente1</li>
+            <li>password: spano</li>
+            <li>username: cliente2</li>
+            <li>password: celentano</li>
+            
+        </ul>
+    </li>
+</ul>
+</p>
+
+</body>
+</html>
